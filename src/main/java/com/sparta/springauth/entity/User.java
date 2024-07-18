4fe90e6faaa1af226d09b88aff5a1b842463bb7a
lgsok00 <lgsok00@naver.com>
2024-07-18 09:14:06 +0900

    User 엔티티

---
+++ src/main/java/com/sparta/springauth/entity/User.java
@@ -0,0 +1,39 @@
+package com.sparta.springauth.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(unique = true, nullable = false)
+  private String username;
+
+  @Column(nullable = false)
+  private String password;
+
+  @Column(unique = true, nullable = false)
+  private String email;
+
+  @Column(nullable = false)
+  @Enumerated(value = EnumType.STRING)  // enum 타입을 DB 컬럼으로 지정할 때 사용
+  private UserRoleEnum role;
+
+}
