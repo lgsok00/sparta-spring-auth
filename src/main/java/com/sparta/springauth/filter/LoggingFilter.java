8f54d3fa7e723128b6d602077e700a9053586c57
lgsok00 <lgsok00@naver.com>
2024-07-28 08:02:10 +0900

    필터 - LoggingFilter

---
+++ src/main/java/com/sparta/springauth/filter/LoggingFilter.java
@@ -0,0 +1,33 @@
+package com.sparta.springauth.filter;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j(topic = "LoggingFilter")
+@Component
+@Order(1)  // 필터의 순서 지정
+public class LoggingFilter implements Filter {
+
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+          throws IOException, ServletException {
+    // 전처리
+    HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+    String url = httpServletRequest.getRequestURI();
+    log.info(url);
+
+    chain.doFilter(request, response);  // 다음 Filter 로 이동
+
+    // 후처리
+    log.info("비즈니스 로직 완료");
+  }
+}
